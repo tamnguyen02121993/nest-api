25b13c4ee6ae05a8efd9b285b247c0a68a2a52bf
Tam Nguyen <tamnguyen02121993@gmail.com>
2024-08-13 16:02:07 +0700

    Update file main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use((req, _, next) => {
+    console.log("Request invoked: ", req.originalUrl)
+    next();
+  })
   await app.listen(8080);
 }
 bootstrap();
