bb1c422eb3f5c036817075256aa55a58e135010c
Tam Nguyen <tamnguyen02121993@gmail.com>
2024-08-21 17:26:02 +0700

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +8,15 @@ async function bootstrap() {
     console.log("Request invoked: ", req.originalUrl)
     next();
   })
+
+  const config = new DocumentBuilder()
+    .setTitle('NestApi example')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(8080);
 }
 bootstrap();
